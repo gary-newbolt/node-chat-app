3acaf2a645a8ec2ccb76868a95c67aef737ed616
= <gary.newbolt@es.catapult.org.uk>
2017-11-20 17:01:46 +0000

    added socket communications

--- server/server.js
+++ server/server.js
@@ -1,19 +1,32 @@
 require('./config/config');
-const path = require('path');
+const path = require('path');//
+const http = require('http');//
+const socketIO = require('socket.io');//
 
-const express = require('express');
-const app = express();
-const port = process.env.PORT;
+const express = require('express');//
+const app = express();//
+const port = process.env.PORT;//
+const server = http.createServer(app);
+const io = socketIO(server);//
 
 
 const publicPath = path.join(__dirname, '../public');
 
 
-// console.log(__dirname + '../public/public');
-// console.log(publicPath);
-
+//command to configure the middleware
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+// register event listener
+io.on('connection', (socket) => {
+    "use strict";
+    console.log('New user connected');
+
+    socket.on('disconnect', () => {
+        console.log('Client disconnected.');
+    });
+});
+
+
+server.listen(port, () => {
     console.log(`Started on port ${port}`);
 });
